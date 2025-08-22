88cd9f03a31c44977a619cc1406983fba8de29fb
Ashutosh Dhakad <ashutoshatoliya@gmail.com>
2025-08-22 15:34:40 +0530

    refactor: update .gitignore and implement image upload endpoint

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const multer = require("multer");
 
 const app = express();
-
 app.use(express.json());
 
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
+
+app.post("/post",upload.single("avatar"), async (req, res) => {
+    try {
+        
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: "something went wrong."
+        });
+    }
+
+})
 
 module.exports = app;
