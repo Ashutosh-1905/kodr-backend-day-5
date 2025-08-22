35d1b95f2b135c9502adb4589b80a032a2878438
Ashutosh Dhakad <ashutoshatoliya@gmail.com>
2025-08-22 16:46:31 +0530

    create post

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,23 +1,43 @@
 const express = require("express");
+const app = express();
+
 const multer = require("multer");
 
-const app = express();
+// const cors = require("cors");
+const uploadFile = require("./services/post.service");
+const postModel = require("./models/post.model");
+// app.use(cors());
 app.use(express.json());
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 
-app.post("/post",upload.single("avatar"), async (req, res) => {
+app.post("/posts",upload.single("avatar"), async (req, res) => {
     try {
-        
+        const caption = req.body.caption;
+        const file = req.file.buffer;
+        const result = await uploadFile(file, "test");
+        if (!result) {
+            return res.status(500).json({
+                message:"result error"
+            })
+        }
+        const post = await postModel.create({
+            caption: caption,
+            url: result.url
+        });
+
+        res.json({
+            message: "post Created successfully",
+            post:post
+        })
     } catch (err) {
         console.log(err);
         res.status(500).json({
-            message: "something went wrong."
+            message: err.message
         });
     }
-
 })
 
 module.exports = app;
