75f5c7ed04e480a9ef5a36a99ba3acc79159ca86
Ashutosh Dhakad <ashutoshatoliya@gmail.com>
2025-08-22 15:40:20 +0530

    feat: create post model with caption and url fields

---
+++ backend/src/models/post.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+    caption: {
+        type: String,
+        required: true,
+    },
+    url: {
+        type: String
+    }
+});
+
+
+const postModel = mongoose.model("post", postSchema);
+
+module.exports = postModel;
